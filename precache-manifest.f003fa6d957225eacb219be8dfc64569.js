d53a38fba4af66bb1a45ef639edb3b3c072199fb
Vishal Ahmad <v-viahma@microsoft.com>
2019-07-17 18:04:00 +0530

    Updates

---
+++ precache-manifest.f003fa6d957225eacb219be8dfc64569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698bc15ee0f250302d14c2702f801fda",
+    "url": "/react-saga-assignment/index.html"
+  },
+  {
+    "revision": "247e0c99942d5deb836f",
+    "url": "/react-saga-assignment/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f416f7d4d639a2bb99f8",
+    "url": "/react-saga-assignment/static/js/2.4318427f.chunk.js"
+  },
+  {
+    "revision": "247e0c99942d5deb836f",
+    "url": "/react-saga-assignment/static/js/main.4a1cb460.chunk.js"
+  },
+  {
+    "revision": "afb52ab7644f083024f1",
+    "url": "/react-saga-assignment/static/js/runtime~main.9572eec0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-saga-assignment/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware  } from "redux";
-
-
-import reducer from "./reducers";
-
-export default function configureStore(middleware) {
-  const store = createStore(reducer, applyMiddleware(middleware));
-  return store;
-}
